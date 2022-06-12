48cb7443bd64371c7ab76fa40ae497aa4e4a0d93
Michele <micheleberardi@pm.me>
2022-06-12 18:31:13 -0400

    sitemap

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ function setColorHoverListener() {
 /* Photos */
 
 function setRandomPhoto() {
-  let num = Math.floor(Math.random() * 3) + 1;
+  let num = Math.floor(Math.random() * 1) + 1;
   document.getElementById("propic").src = `./img/face${num}.jpg`;
 }
 
